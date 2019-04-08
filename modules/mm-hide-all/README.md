7404e911ee6d471fddbbbfbb9023e4a2cc5caa34
kazuyuki-ogata <ogata@itto.co.jp>
2019-04-08 19:33:00 +0900

    mm-hide-allの追加

---
+++ modules/mm-hide-all/README.md
@@ -0,0 +1,21 @@
+MM-Hide-All
+===
+[MagicMirror](https://github.com/MichMich/MagicMirror) Module to hide everything on screen.
+
+Setup:
+---
+* Add the following to your config:
+````javascript
+{
+	module: 'mm-hide-all',
+	position: 'bottom_right'
+},
+````
+
+Once the module is loaded correctly, simple click or tap the 'Hide' button. Click it again to get everything back.
+
+* Note
+
+This requires the ability for you to click the button. It is not voice activated.
+
+I built this as I am personally working on a touch screen MagicMirror of my own that I plan to use this on.
---
+++ modules/mm-hide-all/mm-hide-all.js
@@ -0,0 +1,74 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MM Hide All
+ *
+ * By EoF https://forum.magicmirror.builders/user/eof
+ * MIT Licensed.
+ */
+
+Module.register("mm-hide-all", {
+	getScripts: function () {
+		return ["modules/mm-hide-all/js/jquery.js"];
+	},
+
+	getStyles: function () {
+		return ["mm-hide-all-style.css"];
+	},
+	// magicmirror show/hide
+	showHideMirror: function (isShow) {
+		if (isShow) {
+			$(this.overlay).fadeIn(1000);
+			$(this.button).fadeTo(1000, 0.3);
+			$(this.text).html('Show');
+		} else {
+			$(this.overlay).fadeOut(1000);
+			$(this.button).fadeTo(1000, 1);
+			$(this.text).html('Hide');
+		}
+	},
+	getDom: function () {
+
+		this.wrapper = document.createElement("div");
+		this.button = document.createElement("div");
+		this.text = document.createElement("span");
+		this.overlay = document.createElement("div");
+		this.hidden = true;
+
+		this.overlay.className = "paint-it-black";
+
+		this.button.className = "hide-toggle";
+		this.button.appendChild(this.text);
+		this.text.innerHTML = "Hide";
+
+		this.wrapper.appendChild(this.button);
+		this.wrapper.appendChild(this.overlay);
+
+		var self = this;
+		$(this.button).on("click", function () {
+
+			if (self.hidden) {
+				$(self.overlay).fadeIn(1000);
+				$(self.button).fadeTo(1000, 0.3);
+				$(self.text).html('Show');
+				self.hidden = false;
+			} else {
+				$(self.overlay).fadeOut(1000);
+				$(self.button).fadeTo(1000, 1);
+				$(self.text).html('Hide');
+				self.hidden = true;
+			}
+		});
+
+		return this.wrapper;
+	},
+	notificationReceived: function (notification, payload, sender) {
+		if (notification === "SHOW_MIRROR") {
+			this.showHideMirror(true);
+		}
+
+		if (notification === "SMART_MIRROR") {
+			this.showHideMirror(false);
+		}
+	}
+});
